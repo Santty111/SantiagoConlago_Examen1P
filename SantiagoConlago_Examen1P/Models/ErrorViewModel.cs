103541c6a8c185701f7c69a1b8f877db52f4e561
laboratorio <laboratorio@UPEAULA-31709>
2024-10-28 09:45:12 -0500

    Agregar archivos de proyecto.

---
+++ SantiagoConlago_Examen1P/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SantiagoConlago_Examen1P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SantiagoConlago_Examen1P/Models/SantiagoConlago_model.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SantiagoConlago_Examen1P.Models
+{
+    public class SantiagoConlago_model
+    {
+        public int videojuegoId { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
